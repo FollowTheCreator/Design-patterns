dc4f6e2bc6b094f69ce57ead84c664ef3f1713f7
Artyom <Artsiom.Kavalevich@itechart-group.com>
2019-07-05 16:15:22 +0300

    add abstract factory usage

--- ITechart.Patterns/AbstractFactory/AbstractFactoryUsage.cs
+++ ITechart.Patterns/AbstractFactory/AbstractFactoryUsage.cs
@@ -8,5 +8,18 @@ namespace ITechart.Patterns.AbstractFactory
 {
     class AbstractFactoryUsage
     {
+        public void UseAbstractFactory()
+        {
+            Car MyCar = new Car(new EconomicCarFactory());
+            Console.WriteLine("Economic car description:");
+            MyCar.DescribeCar();
+            MyCar = new Car(new StandartCarFactory());
+            Console.WriteLine("\nStandart car description:");
+            MyCar.DescribeCar();
+            MyCar = new Car(new ExtraCarFactory());
+            Console.WriteLine("\nExtra car description:");
+            MyCar.DescribeCar();
+            Console.ReadKey();
+        }
     }
 }
