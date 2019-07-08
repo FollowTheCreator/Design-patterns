66753a10c77b68de636c0615e383d2dd25b5de1b
Artyom <Artsiom.Kavalevich@itechart-group.com>
2019-07-08 15:05:49 +0300

    add adapter pattern usage

--- ITechart.Patterns/Adapter/AdapterUsage.cs
+++ ITechart.Patterns/Adapter/AdapterUsage.cs
@@ -11,6 +11,16 @@ namespace ITechart.Patterns.Adapter
 {
     class AdapterUsage
     {
-        
+        public static void UseAdapter()
+        {
+            Console.WriteLine(".json input");
+            Book OldestBook = BooksAnalyzer.GetOldestBook(new Json(@"..\..\Adapter\Data\Books.json"));
+            Console.WriteLine($"Author: {OldestBook.Author}, Date of creation: {OldestBook.DateOfCreation}, Name: {OldestBook.Name}");
+
+            Console.WriteLine("\n.xml input");
+            IJson AdaptedXMLList = new AdapterXMLToJson(new XML(@"..\..\Adapter\Data\Books.xml"));
+            OldestBook = BooksAnalyzer.GetOldestBook(AdaptedXMLList);
+            Console.WriteLine($"Author: {OldestBook.Author}, Date of creation: {OldestBook.DateOfCreation}, Name: {OldestBook.Name}");
+        }
     }
 }
