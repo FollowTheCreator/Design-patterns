b7ef3e1f2098f958465e8d7732264dc604628579
Artyom <Artsiom.Kavalevich@itechart-group.com>
2019-07-09 11:25:10 +0300

    add example of proxy pattern usage

--- ITechart.Patterns/Program.cs
+++ ITechart.Patterns/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ITechart.Patterns.AbstractFactory;
+using ITechart.Patterns.Proxy;
 
 namespace ITechart.Patterns
 {
@@ -11,8 +12,9 @@ namespace ITechart.Patterns
     {
         static void Main(string[] args)
         {
-            AbstractFactoryUsage ExampleAbstractFactoryUsage = new AbstractFactoryUsage();
-            ExampleAbstractFactoryUsage.UseAbstractFactory();
+            Console.WriteLine("--Proxy");
+            ProxyUsage.UseProxy();
+            Console.ReadKey();
         }
     }
 }
