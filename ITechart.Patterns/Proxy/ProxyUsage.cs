442a86d991b1f17764aaa1468baad4f911e9dfad
Artyom <Artsiom.Kavalevich@itechart-group.com>
2019-07-09 11:24:21 +0300

    add proxy pattern usage

--- ITechart.Patterns/Proxy/ProxyUsage.cs
+++ ITechart.Patterns/Proxy/ProxyUsage.cs
@@ -11,6 +11,17 @@ namespace ITechart.Patterns.Proxy
 {
     class ProxyUsage
     {
+        public static void UseProxy()
+        {
+            IYesterdayRate rates = new YesterdayRateProxy();
+            ExchangeRate firstRate = rates.GetRate(1);
+            Console.WriteLine($"Rate: {firstRate.Rate}, Source: {firstRate.Source}");
 
+            ExchangeRate secondRate = rates.GetRate(2);
+            Console.WriteLine($"Rate: {secondRate.Rate}, Source: {secondRate.Source}");
+
+            ExchangeRate thirdRate = rates.GetRate(1);
+            Console.WriteLine($"Rate: {thirdRate.Rate}, Source: {thirdRate.Source}");
+        }
     }
 }
