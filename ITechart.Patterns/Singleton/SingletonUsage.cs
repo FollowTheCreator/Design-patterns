1e6eada00bb755a40e7c39e12d228d53c83797cf
Artyom <Artsiom.Kavalevich@itechart-group.com>
2019-07-09 15:52:15 +0300

    add singleton usage

--- ITechart.Patterns/Singleton/SingletonUsage.cs
+++ ITechart.Patterns/Singleton/SingletonUsage.cs
@@ -10,6 +10,30 @@ namespace ITechart.Patterns.Singleton
 {
     class SingletonUsage
     {
+        public static void UseSingleton()
+        {
+            Console.WriteLine("First access:");
+            using (AccessToDB access = AccessToDB.GetAccess())
+            {
+                PeopleOuput(access);
 
+                Console.WriteLine("\nTrying to another access while singleton instance exists:");
+                AccessToDB anotherAccess = AccessToDB.GetAccess();
+            }
+
+            Console.WriteLine("\nTrying to another access after the instance dispose:");
+            using (AccessToDB access = AccessToDB.GetAccess())
+            {
+                PeopleOuput(access);
+            }
+        }
+
+        private static void PeopleOuput(AccessToDB access)
+        {
+            foreach (Person person in access.People)
+            {
+                Console.WriteLine($"Name: {person?.Name}, Age: {person?.Age}");
+            }
+        }
     }
 }
