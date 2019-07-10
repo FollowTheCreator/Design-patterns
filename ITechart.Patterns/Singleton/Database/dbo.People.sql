0e4283361426c0af4d2cbc585774d4776a897837
Artyom <Artyomkovalevich.top@gmail.com>
2019-07-10 16:33:48 +0300

    fix database initializing query

--- ITechart.Patterns/Singleton/Database/dbo.People.sql
+++ ITechart.Patterns/Singleton/Database/dbo.People.sql
@@ -1,6 +1,14 @@
-﻿CREATE TABLE [dbo].[People]
+﻿create database [SingletonDB]
+GO
+use SingletonDB
+GO
+CREATE TABLE [dbo].[People]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NULL, 
     [Age] TINYINT NULL
 )
+GO
+insert into [People] (Name, Age) values ('Alexander', 20), ('Artyom', 20), ('Evgeny', 22), ('Michael', 21)
+GO
+select top 10 * from People
